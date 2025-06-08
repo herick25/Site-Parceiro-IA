0e0c17d96e5ac369770ac4906d4dbfaad21e589e
herick25 <Kevert.herick@gmail.com>
2025-06-07 21:05:05 -0300

    Primeiro commit do site com Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Modules from './components/Modules';
+import Plans from './components/Plans';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Modules />
+      <Plans />
+      <Footer />
+    </>
+  );
+}
