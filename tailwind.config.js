e4baa1d39fd7717971aaf1b61f18cd7dece9e842
herick25 <Kevert.herick@gmail.com>
2025-06-11 20:59:27 -0300

    Corrige build com Tailwind funcionando

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./index.html', './src/*/.{js,jsx,ts,tsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   safelist: ['bg-gray-50'],
   theme: {
     extend: {},
